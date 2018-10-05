a70c34aa72a8e3b8418e28724c08a9316cdb34e9
Ross Scroggs <ross.scroggs@gmail.com>
2018-10-05 11:52:37 -0700

    Fixed bug in gam update group clear members that caused a trap.

--- src/gam.py
+++ src/gam.py
@@ -22,7 +22,7 @@ For more information, see https://github.com/taers232c/GAMADV-X
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.61.10'
+__version__ = u'4.61.11'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
@@ -13687,8 +13687,8 @@ def doUpdateGroups():
         continue
       removeMembers = []
       for member in result:
-        if _checkMemberRoleIsSuspended(member.get(u'email', member[u'id']), validRoles, isSuspended):
-          removeMembers.append(member)
+        if _checkMemberRoleIsSuspended(member, validRoles, isSuspended):
+          removeMembers.append(member.get(u'email', member[u'id']))
       _batchRemoveGroupMembers(group, i, count, removeMembers, Ent.ROLE_MEMBER)
 
 # gam delete groups <GroupEntity>
